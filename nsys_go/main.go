ded9e799217f89691bd73269929e8a4b415f8c37
Bobby Chen <cb168.github@fastmail.com>
2024-07-20 16:45:44 +0000

    [nsys_go] .nsys-rep to Parquet

--- nsys_go/main.go
+++ nsys_go/main.go
@@ -1,24 +1,68 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"time"
 
 	_ "github.com/marcboeker/go-duckdb"
 )
 
 func main() {
-	targetDir := "build"
-
 	// Delete the target build directory.
+	targetDir := "build"
 	if err := os.RemoveAll(targetDir); err != nil {
 		log.Fatal(err)
 	}
 
 	// Create the target build directory.
 	if err := os.MkdirAll(targetDir, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
+	// Profile the target program using nsys.
+	// TODO: Add configuration for the nsys profile command.
+	targetProgramFile := "../vec_add/build/vec_add_gpu"
+	workloadName := "WORKLOAD"
+	name := "nsys"
+	args := []string{
+		"profile",
+		"--cuda-memory-usage=true",
+		fmt.Sprintf("--output=%s/%s.nsys-rep", targetDir, workloadName),
+		"--force-overwrite=true",
+		targetProgramFile,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, name, args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Export profiling data to an nsys SQLite file.
+	// TODO: Add configuration for nsys stats command.
+	name = "nsys"
+	args = []string{
+		"stats",
+		"--report=cuda_gpu_mem_time_sum",
+		"--report=cuda_gpu_mem_size_sum",
+		"--force-export=true",
+		fmt.Sprintf("%s/%s.nsys-rep", targetDir, workloadName),
+	}
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cmd = exec.CommandContext(ctx, name, args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
 	}
 
@@ -36,10 +80,8 @@ func main() {
 	}
 
 	// Initialize an SQLite database as a set of DuckDB tables.
-	inputNsysSqliteFile := "../vec_add/report/vec_add_gpu.sqlite"
-	namespace := "vec_add_gpu"
 	// TODO: Fix SQL injection vulnerability.
-	_, err = db.Exec(fmt.Sprintf(`ATTACH '%s' (TYPE SQLITE); USE %s;`, inputNsysSqliteFile, namespace))
+	_, err = db.Exec(fmt.Sprintf(`ATTACH '%s/%s.sqlite' (TYPE SQLITE); USE %s;`, targetDir, workloadName, workloadName))
 	if err != nil {
 		log.Fatal(err)
 	}
